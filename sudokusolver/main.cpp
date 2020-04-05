b430fd7fd112b56a393c99c19343dfc86250fb03
nazarati <nazarati_maximilian@protonmail.com>
2020-04-05 22:16:15 +0200

    Initial Commit

---
+++ sudokusolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sudokusolver
+//
+//  Created by Maximilian Nazarati on 05.04.20.
+//  Copyright © 2020 Maximilian Nazarati. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
